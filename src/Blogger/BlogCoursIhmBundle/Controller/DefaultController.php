15bd93e6653c2999befc150f5dc6b8b169f63790
GG31 <genevieve.cirera@gmail.com>
2013-05-13 18:36:58 +0200

    création tables : ok, ajouter article : ok, afficher article(s) : ok

--- src/Blogger/BlogCoursIhmBundle/Controller/DefaultController.php
+++ src/Blogger/BlogCoursIhmBundle/Controller/DefaultController.php
@@ -3,7 +3,7 @@
 namespace Blogger\BlogCoursIhmBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Blogger\BlogCoursIhmBundle\Modele\Article;
+use Blogger\BlogCoursIhmBundle\Entity\Article;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
@@ -29,7 +29,7 @@ class DefaultController extends Controller
       	// Cela va afficher la page d'erreur 404 (on pourra personnaliser cette page plus tard d'ailleurs)
       	throw $this->createNotFoundException('Page inexistante (page = '.$page.')');
     	}
-        $articles = array(
+        /*$articles = array(
     array(
       'titre'   => 'Article 1 par défaut dans array.0',
       'id'      => 1,
@@ -48,12 +48,17 @@ class DefaultController extends Controller
       'auteur' => 'M@teo21',
       'contenu' => '+500% sur 1 an, fabuleux. Blabla…',
       'date'    => new \Datetime())
-        );
+        );*/
+
         // Puis modifiez la ligne du render comme ceci, pour prendre en compte nos articles :
         /*$antispam = $this->get('blogger_blog_cours_ihm.Article');
         if($antispam->isSpam()){
           throw new \Exception('SPAM !');
         }*/
+        $entityManager = $this->getDoctrine()->getManager();
+        //$article = $entityManager->find('BloggerBlogCoursIhmBundle:Article', $id);
+        $repository = $entityManager->getRepository('BloggerBlogCoursIhmBundle:Article');
+        $articles = $repository->findAll();
         return $this->render('BloggerBlogCoursIhmBundle:Default:index.html.twig', array(
         'articles' => $articles
         ));
@@ -66,14 +71,20 @@ class DefaultController extends Controller
 
     public function articleAction($id)
     {
-        $article = array(
+        /*$article = array(
           'titre'   => 'Article par défaut de articleAction !',
           'id'      => 1,
           'auteur'  => 'winzou',
           'contenu' => 'Ce weekend était trop bien. Blabla…',
           'date'    => new \Datetime()
-        );
-     
+        );*/
+        $entityManager = $this->getDoctrine()->getManager();
+        //$article = $entityManager->find('BloggerBlogCoursIhmBundle:Article', $id);
+        $repository = $entityManager->getRepository('BloggerBlogCoursIhmBundle:Article');
+        $article = $repository->find($id);
+        if($article==null){
+          throw $this->createNotFoundException('Article[id='.$id.'] inexistant');
+        }
         // Puis modifiez la ligne du render comme ceci, pour prendre en compte nos articles :
         return $this->render('BloggerBlogCoursIhmBundle:Default:article.html.twig', array(
         'article' => $article
@@ -92,7 +103,10 @@ class DefaultController extends Controller
     		$this->get('session')->getFlashBag()->add('notice', 'Article bien enregistré');
     		return $this->render('BloggerBlogCoursIhmBundle:Default:article.html.twig', array('id' => 0));
     	}*/
-
+      $article = new Article('Second !', 'auteur2', 'contenu : LOL');
+      $entityManager = $this->getDoctrine()->getManager();
+      $entityManager->persist($article);
+      $entityManager->flush();
     	return $this->render('BloggerBlogCoursIhmBundle:Default:ajouter.html.twig');
     }
 }
